9d2c61edf7e63cecced128833bcd727e559c9523
David Idris <didwiz83@gmail.com>
2020-06-08 08:02:45 +0100

    added audio tap color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: [],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        "audio-purple": "#9D0B40",
+        "audio-purple": {
+          "100": "#f788b0",
+          "200": "#f35891",
+          "300": "#f24081",
+          "400": "#f02872",
+          "500": "#ee1162",
+          "600": "#d70f58",
+          "700": "#bf0d4e",
+          "800": "#a70c45",
+          "900": "#9D0B40"
+        }
       }
     }
   },
   variants: {},
-  plugins: [require('@tailwindcss/ui')]
+  plugins: [require("@tailwindcss/ui")]
 };
