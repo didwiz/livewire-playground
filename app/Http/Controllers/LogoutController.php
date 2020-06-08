6475834ceee5cdfbabf7e2045612eba1daa1767b
David Idris <didwiz83@gmail.com>
2020-06-08 08:06:13 +0100

    added logout controller

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke()
+    {
+        Auth::logout();
+        return redirect('login')->with('success', 'You have been logged out successfully.');
+    }
+}
